78698025ae14825a77367b1b46c057292966d5ae
Nam Bui <nam.q2bui@gmail.com>
2026-06-14 12:05:02 +1000

    Add roster_diff.py

---
+++ roster_diff.py
@@ -0,0 +1,51 @@
+current = {
+  'ADE': ['R. Thilthorpe','I. Rankine','T. Walker','C. Ah Chee','D. Fogarty','J. Dawson','B. Keays','M. Crouch','R. Laird','W. Milera','J. Worrell','M. Michalanney','J. Butts'],
+  'BRI': ['L. Neale','D. Zorko','H. McCluggage','Z. Bailey','J. Dunkley','C. Cameron','E. Hipwood','J. Daniher','L. McCarthy','O. Allen','D. Fort','S. Draper','J. Fletcher'],
+  'CAR': ['P. Cripps','S. Walsh','G. Hewett','E. Curnow','L. Fogarty','H. McKay','M. Cottrell','A. Saad','M. Pittonet','L. Cowan','B. Kemp','O. Florent','B. Ainsworth'],
+  'COL': ['N. Daicos','J. Daicos','S. Pendlebury','J. Crisp','P. Lipinski','J. Elliott','O. Henry','D. Moore','B. Maynard','B. Frampton','D. Cameron'],
+  'ESS': ['Z. Merrett','D. Parish','N. Martin','X. Duursma','J. Caldwell','P. Wright','K. Langford','W. Snelling','H. Jones','A. McGrath','M. Redman'],
+  'FRE': ['A. Brayshaw','C. Serong','H. Young','L. Jackson','S. Darcy','M. Johnson','J. Amiss','N. Erasmus','S. Bolton','L. Ryan','A. Pearce','H. Chapman'],
+  'GEE': ['J. Cameron','G. Rohan','B. Close','O. Dempsey','P. Dangerfield','B. Smith','T. Atkins','M. Holmes','Z. Guthrie','T. Stewart','J. Kolodjashnij','S. De Koning','R. Stanley','J. Worpel'],
+  'GCS': ['T. Miller','M. Rowell','N. Anderson','J. Ugle-Hagan','B. King','J. Lukosius','C. Graham','J. Walter','W. Powell','S. Collins','C. Petracca'],
+  'GWS': ['L. Whitfield','T. Green','C. Oliver','J. Kelly','J. Hogan','T. Greene','H. Himmelberg','B. Daniels','D. Rioli','S. Taylor','N. Haynes','K. Briggs'],
+  'HAW': ['J. Ward','J. Newcombe','C. Nash','J. Gunston','M. Schache','C. Macdonald','L. Bramble','J. Sicily','K. Amon','J. Impey','L. Meek'],
+  'MEL': ['M. Gawn','J. Viney','J. Steele','C. Salem','T. Sparrow','K. Pickett','T. McDonald','B. Fritsch','J. Van Rooyen','B. Mihocek','J. Lever','S. May','J. Bowey'],
+  'NTH': ['H. Sheezel','L. Davies-Uniacke','C. McKercher','J. Simpkin','L. Parker','T. Powell','B. Scott','N. Larkey','C. Lazzaro','G. Wardlaw','T. Xerri','J. Hoogaard'],
+  'POR': ['Z. Butters','C. Rozee','J. Horne-Francis','O. Wines','W. Drew','S. Powell-Pepper','T. Marshall','M. Georgiades','K. Farrell','D. Houston','J. Finlayson','I. Nankervis'],
+  'RIC': ['T. Taranto','D. Prestia','J. Hopper','J. Ross','T. Lynch','M. Rioli','T. Brown','T. Nankervis','N. Vlastuin','J. Short'],
+  'STK': ['T. De Koning','R. Marshall','J. Macrae','M. Wood','J. Battle','S. Flanders','J. Higgins','M. Owens','M. Phillipou','L. Ryan','J. Sinclair','B. Paton'],
+  'SYD': ['E. Gulden','I. Heeney','C. Mills','J. McInerney','J. Papley','L. McDonald','J. Amartey','T. Blakey','H. McLean','N. Blakey','B. Grundy','C. Curnow','M. Rosas'],
+  'WBD': ['M. Bontempelli','T. Liberatore','A. Treloar','J. Freijah','B. Dale','A. Naughton','R. Garcia','C. Daniel','L. McNeil','S. Darcy','T. English','E. Richards'],
+  'WCE': ['E. Yeo','T. Kelly','H. Reid','J. Waterman','D. Robertson','A. West','B. Williams','T. Barrass','L. Duggan','H. Edwards'],
+}
+
+sc = {
+  'ADE': ['R. Thilthorpe','I. Rankine','T. Walker','C. Ah Chee','D. Fogarty','J. Dawson','B. Keays','R. Laird','W. Milera','J. Worrell','M. Michalanney','J. Butts'],
+  'BRI': ['L. Neale','D. Zorko','H. McCluggage','Z. Bailey','J. Dunkley','C. Cameron','E. Hipwood','L. McCarthy','O. Allen','D. Fort','S. Draper','J. Fletcher'],
+  'CAR': ['P. Cripps','S. Walsh','G. Hewett','L. Fogarty','H. McKay','M. Cottrell','A. Saad','M. Pittonet','L. Cowan','B. Kemp','O. Florent','B. Ainsworth'],
+  'COL': ['N. Daicos','J. Daicos','S. Pendlebury','J. Crisp','P. Lipinski','J. Elliott','D. Moore','B. Maynard','B. Frampton','D. Cameron','D. Houston'],
+  'ESS': ['Z. Merrett','D. Parish','N. Martin','X. Duursma','J. Caldwell','P. Wright','K. Langford','H. Jones','A. McGrath','M. Redman'],
+  'FRE': ['A. Brayshaw','C. Serong','H. Young','L. Jackson','S. Darcy','M. Johnson','J. Amiss','N. Erasmus','S. Bolton','L. Ryan','A. Pearce','H. Chapman'],
+  'GEE': ['J. Cameron','B. Close','O. Dempsey','P. Dangerfield','B. Smith','T. Atkins','M. Holmes','Z. Guthrie','T. Stewart','J. Kolodjashnij','S. De Koning','R. Stanley','J. Worpel','O. Henry'],
+  'GCS': ['T. Miller','M. Rowell','N. Anderson','J. Ugle-Hagan','B. King','C. Graham','J. Walter','W. Powell','S. Collins','C. Petracca'],
+  'GWS': ['L. Whitfield','T. Green','C. Oliver','J. Kelly','J. Hogan','T. Greene','B. Daniels','S. Taylor','K. Briggs'],
+  'HAW': ['J. Ward','J. Newcombe','C. Nash','J. Gunston','C. Macdonald','J. Sicily','K. Amon','J. Impey','L. Meek','T. Barrass','J. Battle'],
+  'MEL': ['M. Gawn','J. Viney','J. Steele','C. Salem','T. Sparrow','K. Pickett','T. McDonald','B. Fritsch','J. Van Rooyen','B. Mihocek','J. Lever','S. May','J. Bowey'],
+  'NTH': ['H. Sheezel','L. Davies-Uniacke','C. McKercher','J. Simpkin','L. Parker','T. Powell','B. Scott','N. Larkey','G. Wardlaw','T. Xerri'],
+  'POR': ['Z. Butters','C. Rozee','J. Horne-Francis','O. Wines','W. Drew','S. Powell-Pepper','T. Marshall','M. Georgiades','K. Farrell'],
+  'RIC': ['T. Taranto','D. Prestia','J. Hopper','J. Ross','T. Lynch','M. Rioli','T. Brown','T. Nankervis','N. Vlastuin','J. Short'],
+  'STK': ['T. De Koning','R. Marshall','J. Macrae','M. Wood','S. Flanders','J. Higgins','M. Owens','M. Phillipou','L. Ryan','J. Sinclair'],
+  'SYD': ['E. Gulden','I. Heeney','C. Mills','J. McInerney','T. Papley','L. McDonald','J. Amartey','T. Blakey','H. McLean','N. Blakey','B. Grundy','C. Curnow','M. Rosas'],
+  'WBD': ['M. Bontempelli','T. Liberatore','A. Treloar','J. Freijah','B. Dale','A. Naughton','R. Garcia','L. McNeil','S. Darcy','T. English','E. Richards','L. Bramble'],
+  'WCE': ['E. Yeo','T. Kelly','H. Reid','J. Waterman','D. Robertson','B. Williams','L. Duggan','H. Edwards'],
+}
+
+print('=== CHANGES NEEDED ===')
+for team, players in current.items():
+    sc_players = sc.get(team, [])
+    to_remove = [p for p in players if p not in sc_players]
+    to_add = [p for p in sc_players if p not in players]
+    if to_remove or to_add:
+        print(f'\n{team}:')
+        for p in to_remove: print(f'  REMOVE: {p}')
+        for p in to_add:    print(f'  ADD:    {p}')
